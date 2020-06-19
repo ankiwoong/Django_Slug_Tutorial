71253b8a955e82158d2abe13e3f8ec2449e02485
Ankiwoong <ankiwoong@gmail.com>
2020-06-19 09:17:23 +0900

    Slugs URLs

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path("<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
-    path("", ArticleListView.as_view(), name="article_list"),
-]
+    path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'), # new
+    path('', ArticleListView.as_view(), name='article_list'),
