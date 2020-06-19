53ec56cfbefed00a5cdc4ae56902660899b7e760
Ankiwoong <ankiwoong@gmail.com>
2020-06-19 09:05:43 +0900

    Article Model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("article_detail", args=[str(self.id)])
+
