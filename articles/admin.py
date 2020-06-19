3ced0ef86564eff4862dc65e992e8e579d43bc16
Ankiwoong <ankiwoong@gmail.com>
2020-06-19 09:08:29 +0900

    Django Admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "body",
+    )
+
+
+admin.site.register(Article, ArticleAdmin)
