62d9340d21be71f201143e80aa6588d8c4d75dd8
Ankiwoong <ankiwoong@gmail.com>
2020-06-19 09:18:16 +0900

    Fix

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('<slug:slug>', ArticleDetailView.as_view(), name='article_detail'), # new
-    path('', ArticleListView.as_view(), name='article_list'),
+    path("<slug:slug>", ArticleDetailView.as_view(), name="article_detail"),  # new
+    path("", ArticleListView.as_view(), name="article_list"),
+]
