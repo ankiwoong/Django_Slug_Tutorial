fc514c8f737a269a31e85dcbd8fc9b73680cc13b
Ankiwoong <ankiwoong@gmail.com>
2020-06-19 09:30:12 +0900

    Signals, Lifecycle Hooks, Save, and Forms/Serializers

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.template.defaultfilters import slugify  # new
 from django.urls import reverse
 
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    slug = models.SlugField(null=False, unique=True)  # new
+    slug = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return self.title
@@ -13,3 +14,7 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"slug": self.slug})
 
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
